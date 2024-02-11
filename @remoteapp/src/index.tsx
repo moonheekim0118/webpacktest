21fabf58652c6d78abaf2c02abd0dc860f30f9d8
moonheekim0118 <hellomooneekim@gmail.com>
2024-02-11 17:31:37 +0900

    <chore> remoteapp 추가

---
+++ @remoteapp/src/index.tsx
@@ -0,0 +1,13 @@
+import App from "./App";
+
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
